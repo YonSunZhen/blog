577fbc227d00d400407b70115cb4e2458828bb63
YonSunZhen <1315976441@qq.com>
2019-03-02 17:18:04 +0800

    successBlog_webPage

---
+++ blog_web/src/components/articleList.vue
@@ -0,0 +1,128 @@
+<template>
+  <div id="articleList">
+    <div class="item">
+      <div class="title">
+        <a href="">计算机与信息工程学院召开新学期全体教职工大会暨理论学习会</a>
+      </div>
+      <div class="info">
+        <ul>
+          <li>
+            <p>发布 2018-11-16</p>
+          </li>
+          <li>
+            <p>更新 2018-12-01</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="item">
+      <div class="title">
+        <a href="">计算机与信息工程学院召开新学期全体教职工大会暨理论学习会</a>
+      </div>
+      <div class="info">
+        <ul>
+          <li>
+            <p>发布 2018-11-16</p>
+          </li>
+          <li>
+            <p>更新 2018-12-01</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="item">
+      <div class="title">
+        <a href="">计算机与信息工程学院召开新学期全体教职工大会暨理论学习会</a>
+      </div>
+      <div class="info">
+        <ul>
+          <li>
+            <p>发布 2018-11-16</p>
+          </li>
+          <li>
+            <p>更新 2018-12-01</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="item">
+      <div class="title">
+        <a href="">计算机与信息工程学院召开新学期全体教职工大会暨理论学习会</a>
+      </div>
+      <div class="info">
+        <ul>
+          <li>
+            <p>发布 2018-11-16</p>
+          </li>
+          <li>
+            <p>更新 2018-12-01</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="item">
+      <div class="title">
+        <a href="">计算机与信息工程学院召开新学期全体教职工大会暨理论学习会</a>
+      </div>
+      <div class="info">
+        <ul>
+          <li>
+            <p>发布 2018-11-16</p>
+          </li>
+          <li>
+            <p>更新 2018-12-01</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="item">
+      <div class="title">
+        <a href="">计算机与信息工程学院召开新学期全体教职工大会暨理论学习会</a>
+      </div>
+      <div class="info">
+        <ul>
+          <li>
+            <p>发布 2018-11-16</p>
+          </li>
+          <li>
+            <p>更新 2018-12-01</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'articleList',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+  @import "../assets/css/public.styl"
+
+  .item
+    width 600px
+    margin 23px 0px
+    border-bottom 1px dashed #d9d9d9
+    .title
+      a
+        color #555555
+        font-size 18px
+        font-weight bold
+        line-height 1.5
+    .info
+      overflow auto
+      li
+        float left
+        margin 8px 8px 8px 0px
+        font-size 12px
+        color #717171
+
+
+</style>
